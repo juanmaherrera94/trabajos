869fe222aa532d5e4d7c68cda0d988949a12cb14
juanmaherrera94 <juanmah2911@gmail.com>
2023-10-05 11:53:48 +0200

    2º commit actividad2

--- UD2/actividad2.js
+++ UD2/actividad2.js
@@ -1,3 +1,20 @@
-const carcularSalario=(horasTrabajadas,precio)=>{
-    horasNormales=Math.min(horasTrabajadas,35);
-}
+const calcularSalarioNeto=(horasTrabajadas, precioPorHora) => {
+    const horasNormales = Math.min(35, horasTrabajadas);
+    const horasExtras = Math.max(0, horasTrabajadas - 35);
+    
+    const salarioBruto = (horasNormales * precioPorHora) + (horasExtras * precioPorHora * 1.5);
+    
+    let impuestos = 0;
+    if (salarioBruto > 500) {
+      impuestos += 400 * 0.25;
+      impuestos += (salarioBruto - 500)
+    } else if (salarioBruto > 100) {
+      impuestos += (salarioBruto - 100) 
+    }
+    
+    const salarioNeto = salarioBruto - impuestos;
+    
+    console.log("Salario neto semanal: " + salarioNeto + " euros");
+  }
+
+  calcularSalarioNeto(40, 15);
