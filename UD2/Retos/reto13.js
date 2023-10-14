98946bea65ca574da012a050dfab92210a8fde5d
juanmaherrera94 <juanmah2911@gmail.com>
2023-10-14 11:22:21 +0200

    archivos eliminados

--- UD2/Retos/reto13.js
+++
@@ -1,9 +0,0 @@
-const categorias=["Geografia","Arte","Espectaculos","Historia","Ciencias","Deportes"];
-const preguntas={
-    Geografia:["¿Cuál es la capital de Francia?"],
-    Arte:["¿Quién pintó la famosa obra antibelicista 'Guernica'?"],
-    Espectaculos:["De donde proviene el reggae"],
-    Historia:["¿Quien descubrio America?"],
-    Ciencias:["¿Cual es el cuerpo mas grande del cuerpo humano?"],
-    Deportes:["¿Que equipo le retiro el dorsal al jugador Jude Bellingham?"]
-}
