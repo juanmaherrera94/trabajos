19a13579b073d1878ecbb4c2503321825c8e7247
juanmaherrera94 <juanmah2911@gmail.com>
2023-10-25 11:59:20 +0200

    commit2 ejer1teoria

--- UD3/ejer1teoria.js
+++ UD3/ejer1teoria.js
@@ -16,10 +16,28 @@ class Productos{
     }
    
 }
-const producto1 = new Productos("Producto 1","Tecnologia",80,100);
+/*const producto1 = new Productos("Producto 1","Tecnologia",80,100);
 const producto2 = new Productos("Producto 2","Pescado",20,4.95);
 const producto3 = new Productos("Producto A","Dulces",6,2.95);
+
+ 
 
 console.log(producto1.getInfo());
 console.log(producto2.getInfo());
 console.log(producto3.getInfo());
+**/
+class Televisores extends Productos{
+    constructor(name,category,units,price,tamanio){
+        super(name,category,units,price);
+        this.tamanio=tamanio;
+    }
+    getInfo(){
+        const total=this.total();
+        return `${this.name} (${this.category}), ${this.tamanio} pulgadas: ${this.units} unidades x ${this.price} € = ${total} €`;
+        
+    }
+}
+const producto1=new Televisores("Televisor Lg","Televisores",5,600,65);
+const producto2=new Televisores("Televisor Xiaomi","Televisores",2,1000,55);
+console.log(producto1.getInfo());
+console.log(producto2.getInfo());
