814acab6ae6f594b1686cd0017502bc90bbb8319
juanmaherrera94 <juanmah2911@gmail.com>
2023-10-25 16:59:15 +0200

    commit ejercicios

---
+++ UD3/ejemploAlejandro.js
@@ -0,0 +1,15 @@
+const arrayNumeros=[1,2,3,20,45,55,63];
+sum=0;
+for (const valor of arrayNumeros) {
+    sum+=valor;
+}
+array.forEach(element => {
+    sum+=element;
+});
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
---
+++ UD3/ejemplosArray.js
@@ -0,0 +1,22 @@
+const listaCompra=["Peras","Manzanas","Kiwis","Platanos","Mandarinas"];
+let borrar=listaCompra.splice(1,1);
+console.log(listaCompra);
+let aniadir=listaCompra.splice(3,0,"Naranjas","Sandia");
+console.log(listaCompra);
+let borrarKiwis=listaCompra.slice(1,1,"Cerezas","Nisperos");
+console.log(listaCompra);
+
+const arrayNotas=[4,8,3,10,5];
+let ordenarNotas=arrayNotas.sort((nota1,nota2) => nota1-nota2);
+console.log(arrayNotas);
+
+const diaDeLaSemana=
+["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+let empiezanPorM=diaDeLaSemana.filter(letraM => letraM[0]==="M");
+console.log(empiezanPorM);
+let dia=diaDeLaSemana.find(dias=>dias.startsWith("M"));
+console.log(dia);
+let dia1=diaDeLaSemana.every(dias=> dias.startsWith("S"));
+let dia2=diaDeLaSemana.some(dias=> dias.endsWith("S"));
+let mayusculas=diaDeLaSemana.map(mayus=>mayus.toUpperCase());
+console.log(mayusculas);
